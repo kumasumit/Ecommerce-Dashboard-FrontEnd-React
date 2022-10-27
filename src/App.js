0c741de096df1156c774e2cf2d09c0ea05834717
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-27 17:42:43 +0530

    complete nav bar routing and links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-     <h1> Ecommerce Dashboard </h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          {/* all routes within a common route defined on Nav will be defined here */}
+          <Route path="/" element={<h1>Product Component</h1>} />
+          <Route path="/add" element={<h1> AddProduct Component</h1>} />
+          <Route path="/update" element={<h1>Update Product Component</h1>} />
+          <Route path="/logout" element={<h1> Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
