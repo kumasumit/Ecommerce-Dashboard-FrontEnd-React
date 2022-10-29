64f307cc889888ec41c03bf73e2b2bb6d9b63038
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-30 01:18:33 +0530

    Integrate search api functionality

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -31,10 +31,36 @@ const ProductList = () => {
             getProducts();
         }
     }
+    // Implement the searchHandle function
+    const searchHandle = async (event)=>{
+        let key = event.target.value;
+        if(key){
+            // ask rahul >> ?? 
+            // to convert fetch into axios request
+            let result = await fetch(`http://localhost:8000/search/${key}`);
+            result = await result.json()
+            if(result){
+                setProducts(result)
+            }
+        }else{
+            //if there is nothing in search box
+            //call the getProducts() function
 
+            getProducts();
+        }
+
+    }
     return (
         <div className="product-list">
             <h3>Product List</h3>
+            <input type="" className='search-product-box' placeholder='Search Product'
+            onChange={searchHandle}
+             />
+             {/* here as soon as search box content changes, the function searchHandle is called */}
+             {/* ask rahul >>> ????
+             how to memoize this search by debouncing and throttling */}
+
+
             <ul>
                 <li>S.No.</li>
                 <li>Name</li>
@@ -42,8 +68,9 @@ const ProductList = () => {
                 <li>Category</li>
                 <li>Operation</li>
             </ul>
-            {
-                products.map((item, index) =>
+            { 
+            // if product array length is greater than 0 display the list else show no products found
+                products.length>0 ? products.map((item, index) =>
 
                     <ul key={item._id}>
                         {/* index starts from 0 so we do index+1 so that index starts from 1     */}
@@ -57,6 +84,7 @@ const ProductList = () => {
                         <Link to={"/update/"+item._id} >Update </Link></li>
                     </ul>
                 )
+                :<h1>No Result Found</h1>
             }
         </div>
     )
