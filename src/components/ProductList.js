b698e10e9a8ec0e5b17aabd8efd78e5844cff4d4
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-29 21:03:01 +0530

    Integrate Delete Product Api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -18,27 +18,40 @@ const ProductList = () => {
     }
 
 
-    console.log(products);
+    const deleteProduct = async (id) => {
+        console.log(id);
+        // ask rahul >>>> ????
+        // convert fetch into axios
+        let result = await fetch(`http://localhost:8000/product/${id}`, {
+            method: "Delete"
+        });
+        result = await result.json();
+        if (result) {
+            getProducts();
+        }
+    }
 
     return (
         <div className="product-list">
             <h3>Product List</h3>
             <ul>
-                <li>S. No.</li>
+                <li>S.No.</li>
                 <li>Name</li>
                 <li>Price</li>
                 <li>Category</li>
+                <li>Operation</li>
             </ul>
             {
-                products.map((item,index)=>
+                products.map((item, index) =>
 
-                <ul key={item}>
-                {/* index starts from 0 so we do index+1 so that index starts from 1     */}
-                <li>{index+1}</li>
-                <li>{item.name}</li>
-                <li>{item.price}</li>
-                <li>{item.category}</li>
-            </ul>
+                    <ul key={item._id}>
+                        {/* index starts from 0 so we do index+1 so that index starts from 1     */}
+                        <li>{index + 1}</li>
+                        <li>{item.name}</li>
+                        <li>{item.price}</li>
+                        <li>{item.category}</li>
+                        <li><button onClick={() => deleteProduct(item._id)}>Delete</button></li>
+                    </ul>
                 )
             }
         </div>
