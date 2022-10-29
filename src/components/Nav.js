12c83360bb20a20ab091ba643c95dd8fdb60f19d
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-29 13:08:54 +0530

    implement the logot functionality in frontend

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Nav = () => {
   const auth = localStorage.getItem('user');
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate('/signup');
+  }
   return (
     
     <div>
@@ -14,7 +19,7 @@ const Nav = () => {
                <li><Link to="/update"> Update Products</Link></li>
                <li><Link to="/profile">Profile</Link></li>
                {/* if auth exists means if user exists in localstorage show the logout button else show the SignUp Button */}
-               <li>{auth?<Link to="/logout">Logout</Link>:<Link to="/signup">Sign Up</Link>}</li>
+               <li>{auth?<Link onClick={logout} to="/signup">Logout</Link>:<Link to="/signup">Sign Up</Link>}</li>
         </ul>
 
     </div>
