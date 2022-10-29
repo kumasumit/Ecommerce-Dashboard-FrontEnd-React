50ddb8b3fc00cade18dced6de20bad2aaab4f9b7
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-29 21:33:09 +0530

    Complete Update UI Component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PrivateComponent from "./components/PrivateComponent";
 import Login from "./components/Login";
 import AddProduct from "./components/AddProduct";
 import ProductList from "./components/ProductList";
+import UpdateProduct from "./components/UpdateProduct";
 function App() {
   return (
     <div className="App">
@@ -19,7 +20,7 @@ function App() {
             {/* all the components that require sign-in/login to access is protected under Private Component */}
             <Route path="/" element={<ProductList/>} />
             <Route path="/add" element={<AddProduct/>} />
-            <Route path="/update" element={<h1>Update Product Component</h1>} />
+            <Route path="/update/:id" element={<UpdateProduct/>} />
             <Route path="/logout" element={<h1> Logout Component</h1>} />
             <Route path="/profile" element={<h1>Profile Component</h1>} />
           </Route>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
@@ -50,7 +51,10 @@ const ProductList = () => {
                         <li>{item.name}</li>
                         <li>{item.price}</li>
                         <li>{item.category}</li>
-                        <li><button onClick={() => deleteProduct(item._id)}>Delete</button></li>
+                        <li><button onClick={() => deleteProduct(item._id)}>Delete</button>
+                        {/* ask rahul >> ???
+                        how to change this to template literal */}
+                        <Link to={"/update/"+item._id} >Update </Link></li>
                     </ul>
                 )
             }
